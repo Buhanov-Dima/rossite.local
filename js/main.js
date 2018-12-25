9fab7ed5e9c19fe65d5202ed819104b28f781e33
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-12-25 22:58:29 +0300

    The slider is worked)
    короч заработал слайдер при клике переходит на нужный, пришлось добавить вертикальный скролл, чтобы карусель не сьезжала. Задержка при демонстрации 5 сек

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
-  $(".slider").owlCarousel({
-  	loop:true,
-  	items: 1,
-  	nav: true,
-  	dots: false
-  });
+  
+  	$(".slider").owlCarousel({
+	  	items:1,
+	    loop:true,
+	    nav:true,
+	    dots:false,
+	    center:true,
+	    margin:10,
+	    URLhashListener:true,
+	    autoplayHoverPause:true,
+	    startPosition: 'URLHash'
+	  });
+
+
+  	$('.btn').on('click', function() {
+  		setTimeout(function() {
+       		$('.hide_btn').addClass("hidden");
+  			$('.open_block').removeClass("hidden");
+   			}, 5000);
+  	});
 });
